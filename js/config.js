985c669db1ee7d33a2f6803fc0868acecd5f607c
rahulreddy2080 <vaka.venkata2022@vitstudent.ac.in>
2025-10-30 12:36:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_kHHiHEXWb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6ck7koqjdvunukdhac8mg9ullq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_al9A4ytmZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1b0mjjrrjih0qr94jo09sinmqg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
